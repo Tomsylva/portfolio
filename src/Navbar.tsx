d162762b765b203164e9ad7630eb680ca373cd3e
Tomsylva <tomsylvanz@gmail.com>
2021-08-02 10:54:50 +0200

    dummy links put in

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,9 +2,9 @@ function Navbar() : JSX.Element {
     return (
         <nav>
             <ul>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Projects</a></li>
+                <li><a href="https://tom-sylva.netlify.app/">Contact</a></li>
+                <li><a href="https://tom-sylva.netlify.app/">About</a></li>
+                <li><a href="https://tom-sylva.netlify.app/">Projects</a></li>
             </ul>
         </nav>
     )
