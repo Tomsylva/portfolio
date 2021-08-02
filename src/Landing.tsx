0ac4eaf67d74cf3a612d974e2fe8db74207445a0
Tomsylva <tomsylvanz@gmail.com>
2021-08-02 10:32:28 +0200

    navbar styled

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,9 +1,11 @@
+import {motion} from "framer-motion"
+
 function Landing() : JSX.Element {
     return (
         <div id="home-page">
             <h1 id="home-h1">Tom Sylva</h1>
             <h3 id="home-h3">Web Developer</h3>
-            <img src="./tom.png" id="home-image" alt="pixelated Tom"/>
+            <motion.img src="./tom.png" id="home-image" alt="pixelated Tom"/>
             <br />
         </div>
     )
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,13 +1,12 @@
 function Navbar() : JSX.Element {
     return (
-        <>
-            {/* <ul>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul> */}
-            <h3>Nav</h3>
-        </>
+        <nav>
+            <ul>
+                <li>Contact</li>
+                <li>About</li>
+                <li>Projects</li>
+            </ul>
+        </nav>
     )
 }
 
