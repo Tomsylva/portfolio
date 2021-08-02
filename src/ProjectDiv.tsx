1c86f8373bfad7a23117d1d5d9f9501e724724c0
Tomsylva <tomsylvanz@gmail.com>
2021-08-02 09:21:36 +0200

    some css styling

--- src/ProjectDiv.tsx
+++ src/ProjectDiv.tsx
@@ -7,7 +7,7 @@ interface ProjectDivProps {
 function ProjectDiv({name, link, image} : ProjectDivProps) : JSX.Element {
     return (
         <div className="project-div" style={{backgroundImage: `url(${image})`}}>
-            <a href={link} target="_blank" rel="noreferrer"><h3>{name}</h3></a>
+            <a href={link} target="_blank" rel="noreferrer"><h3 className="project-link">{name}</h3></a>
         </div>
     )
 }
