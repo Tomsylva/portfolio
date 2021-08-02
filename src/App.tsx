c2dad385bb12ece945d012a6d1f2735554363062
Tomsylva <tomsylvanz@gmail.com>
2021-08-02 11:17:24 +0200

    some basic animation added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Landing from './Landing';
 import Navbar from './Navbar';
 import Projects from './Projects';
+import {motion} from "framer-motion"
 
 function App() : JSX.Element {
 
@@ -16,7 +17,13 @@ function App() : JSX.Element {
     <div className="App">
       {homePage ? <div id="home-page-div">
         <Landing/>
-        <button onClick={begin} className="start-button">&lt; Portfolio /&gt;</button>
+        <motion.button initial="hidden"
+      animate="visible"
+      transition={{duration: 1.5}}
+      variants={{
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+      }} onClick={begin} className="start-button">&lt; Portfolio /&gt;</motion.button>
       </div> : <>
         <Navbar />
         <Projects />
--- src/Landing.tsx
+++ src/Landing.tsx
@@ -2,12 +2,20 @@ import {motion} from "framer-motion"
 
 function Landing() : JSX.Element {
     return (
-        <div id="home-page">
+        <motion.div
+      initial="hidden"
+      animate="visible"
+      transition={{duration: 0.75}}
+      variants={{
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+      }}
+    id="home-page">
             <h1 id="home-h1">Tom Sylva</h1>
             <h3 id="home-h3">Web Developer</h3>
             <motion.img src="./tom.png" id="home-image" alt="pixelated Tom"/>
             <br />
-        </div>
+        </motion.div>
     )
 }
 
