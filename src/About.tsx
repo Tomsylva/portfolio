7c21ddc79c25968f24a5eae28eeaf496de5a19b4
Tomsylva <tomsylvanz@gmail.com>
2021-08-04 14:02:35 +0200

    i found a bunch of logos

--- src/About.tsx
+++ src/About.tsx
@@ -3,12 +3,13 @@ function About() : JSX.Element {
         <div>
            <h1>About</h1>
            <p>Full stack web developer based in Nürnberg, Germany.</p>
-           <p><strong>Frameworks & Libraries: </strong>React &#8226; Angular &#8226; Handlebars &#8226; Bootstrap</p>
-           <p><strong>Frontend: </strong>TypeScript &#8226; HTML &#8226; CSS || SASS &#8226; JavaScript(ES6)</p>
-           <p><strong>Backend: </strong>ExpressJS &#8226; NodeJS &#8226; Axios</p>
-           <p><strong>Database: </strong>SQL &#8226; MongoDB</p>
-           <p><strong>Version Control: </strong>Git &#8226; GitHub</p>
-           <i className="fas fa-coffee"></i><i className="fas fa-bicycle"></i><i className="fas fa-mountain"></i><i className="fas fa-guitar"></i><i className="fas fa-laptop"></i>
+           <p>IronHack Web Development Bootcamp graduate.</p>
+           <p><strong>Frameworks & Libraries: </strong>React <i className="fab fa-react"></i> Angular <i className="fab fa-angular"></i> Bootstrap <i className="fab fa-bootstrap"></i> Handlebars</p>
+           <p><strong>Frontend: </strong>HTML <i className="fab fa-html5"></i> CSS <i className="fab fa-css3-alt"></i> SASS <i className="fab fa-sass"></i> JavaScript(ES6) <i className="fab fa-js"></i> TypeScript</p>
+           <p><strong>Backend: </strong>NodeJS <i className="fab fa-node-js"></i> ExpressJS &#8226; Axios</p>
+           <p><strong>Database: </strong>SQL <i className="fas fa-database"> </i> MongoDB</p>
+           <p><strong>Version Control: </strong>Git <i className="fab fa-github"></i> GitHub</p>
+           <p className="icons"><i className="fas fa-coffee"></i><i className="fas fa-bicycle"></i><i className="fas fa-mountain"></i><i className="fas fa-guitar"></i><i className="fas fa-code"></i></p>
         </div>
     )
 }
